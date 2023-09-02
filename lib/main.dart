db72fb570e42c676c29c03826779e79b1990aa8d
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-02 19:53:56 +0530

    list integration

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List <Icon> scoreKeeper = [
+    Icon(
+      Icons.check, color: Colors.green),
+    Icon(
+      Icons.close, color: Colors.red),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -90,12 +96,7 @@ class _QuizPageState extends State<QuizPage> {
         ),
         //TODO: Add a Row here as your score keeper
         Row(
-          children: [
-            Icon(
-               Icons.check, color: Colors.green),
-            Icon(
-              Icons.close, color: Colors.red),
-          ],
+          children: scoreKeeper,
         )
 
 
