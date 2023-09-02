1a06b04609a1b7a6528f54b3081e03a68bc0f9fc
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-02 23:11:55 +0530

    validation

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,19 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [
-    Icon(Icons.check, color: Colors.green),
+    //Icon(Icons.check, color: Colors.green),
     Icon(Icons.close, color: Colors.red),
   ];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> answers = [false, true, true];
+
+  int questionnumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -41,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionnumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -67,7 +77,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  bool correctAnswer = answers[questionnumber];
+                  if(correctAnswer == true)
+                  {
+                    print('user got it right');
+                  }
+                  else
+                  {
+                    print('user got it wrong');
+                  }
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                  questionnumber++;
                   //The user picked true.
                 });
                 
@@ -92,7 +112,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  bool correctAnswer = answers[questionnumber];
+                  if(correctAnswer == false)
+                  {
+                    print('user got it right');
+                  }
+                  else
+                  {
+                    print('user got it wrong');
+                  }
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  questionnumber++;
                 });
                 //The user picked false.
               },
