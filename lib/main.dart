280745cdb66ed8380732134c902892a5e9678fbb
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-03 15:53:47 +0530

    question class created

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class _QuizPageState extends State<QuizPage> {
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
     Question(q: 'A slug\'s blood is green.', a: true),
+    Question(q: 'Your Heisenburg', a: true),
   ];
   int questionnumber = 0;
   @override
