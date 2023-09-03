e5fd9ec9ac2e4522cd4f0d1d248995c0a5fb14c0
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-03 14:30:12 +0530

    classes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+
 
 void main() => runApp(Quizzler());
 
@@ -27,20 +29,18 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [
     //Icon(Icons.check, color: Colors.green),
-    Icon(Icons.close, color: Colors.red),
+    //Icon(Icons.close, color: Colors.red),
   ];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
-
-  List<bool> answers = [false, true, true];
-
   int questionnumber = 0;
   @override
   Widget build(BuildContext context) {
+    
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -51,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionnumber],
+                questionBank[questionnumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,20 +77,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = answers[questionnumber];
-                  if(correctAnswer == true)
-                  {
+                  bool correctAnswer = questionBank[questionnumber].questionAnswer;
+                  if (correctAnswer == true) {
                     print('user got it right');
-                  }
-                  else
-                  {
+                  } else {
                     print('user got it wrong');
                   }
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
                   questionnumber++;
+                
                   //The user picked true.
                 });
-                
+
                 //The user picked true.
               },
             ),
@@ -112,13 +110,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = answers[questionnumber];
-                  if(correctAnswer == false)
-                  {
+                  bool correctAnswer = questionBank[questionnumber].questionAnswer;
+                  if (correctAnswer == false) {
                     print('user got it right');
-                  }
-                  else
-                  {
+                  } else {
                     print('user got it wrong');
                   }
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red));
@@ -137,6 +132,8 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
+
+
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
