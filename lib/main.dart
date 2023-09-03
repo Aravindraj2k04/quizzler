3d7b94fd3dc2bb7cd775379258233a4b6a91d8d4
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-03 16:14:18 +0530

    abstraction implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
-
+import 'package:quizzler/questionbrain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -32,12 +31,7 @@ class _QuizPageState extends State<QuizPage> {
     //Icon(Icons.close, color: Colors.red),
   ];
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-    Question(q: 'Your Heisenburg', a: true),
-  ];
+  questionbrain qbrain = questionbrain();
   int questionnumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -52,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionnumber].questionText,
+               qbrain.questionBank[questionnumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +72,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = questionBank[questionnumber].questionAnswer;
+                  bool correctAnswer = qbrain.questionBank[questionnumber].questionAnswer;
                   if (correctAnswer == true) {
                     print('user got it right');
                   } else {
@@ -111,7 +105,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = questionBank[questionnumber].questionAnswer;
+                  bool correctAnswer = qbrain.questionBank[questionnumber].questionAnswer;
                   if (correctAnswer == false) {
                     print('user got it right');
                   } else {
